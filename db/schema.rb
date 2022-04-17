181d45af16a07959d9e4d62cb79e7e615585c939
Sandra <sandra.moutous@gmail.com>
2022-04-17 19:07:51 +0200

    Add devise users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_17_150048) do
+ActiveRecord::Schema.define(version: 2022_04_17_152411) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,18 @@ ActiveRecord::Schema.define(version: 2022_04_17_150048) do
     t.bigint "cuisine_id"
     t.index ["category_id"], name: "index_restaurants_on_category_id"
     t.index ["cuisine_id"], name: "index_restaurants_on_cuisine_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
